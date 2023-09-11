6596c4d87cec3b9017baa8e4c83cb3b035cb5514
Andrey Shpak <anshpak.by@gmail.com>
2023-09-11 10:37:16 +0300

    feat: add a solotion to the first task

---
+++ tasks/linear-programs/src/FirstTask.java
@@ -0,0 +1,21 @@
+public class FirstTask {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("No arguments were given.");
+            System.exit(0);
+        } else if (args.length > 3) {
+            System.out.println("Warning. Some of the given arguments will not be used because only first three of them are needed.");
+        }
+        try {
+            double a = Double.parseDouble(args[0]);
+            double b = Double.parseDouble(args[1]);
+            double c = Double.parseDouble(args[2]);
+            double z = (a - 3) * b / 2 + c;
+            System.out.println("The result is: " + z + ".");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Not enough arguments were given, three are required.");
+        } catch (NumberFormatException e ) {
+            System.out.println("Non numerical arguments were given.");
+        }
+    }
+}
